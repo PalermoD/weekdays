5c3154bb35b55b3bc05a0c2c9398024a1731fd1d
PalermoD <palermo.deschamps@gmail.com>
2015-11-03 19:13:22 -0500

    first commit

---
+++ index.ios.js
@@ -0,0 +1,24 @@
+// Create a react componet
+var Weekdays = React.createClass({
+	//pass an object to this method
+	// set of instructions that define our component and stucture 
+	// every react componet must have a render function/method 
+    // jsx
+    render: function(){
+    	return <View>
+               <Text>
+               Here are the Days of the Week
+               </Text>
+    	</View>
+    	
+    }
+
+});
+
+
+
+
+
+
+
+// Show the react component on the screen
